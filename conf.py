b49d0885479a84e9f575b8173dc1af96e7cf3d3a
Brad Miller <brad@runestoneinteractive.com>
2019-08-16 14:47:00 +0000

    fix title

--- conf.py
+++ conf.py
@@ -171,7 +171,7 @@ html_theme_path = [pkg_resources.resource_filename('runestone', 'common/project_
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'Problem Solving with Algorithms and Data Structures'
+html_title = 'Problem Solving with Algorithms and Data Structures using C++'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 html_short_title ='Problem Solving with Algorithms and Data Structures'
